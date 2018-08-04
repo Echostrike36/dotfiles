13a8d6e93d4899fb139f801bf8a4d7be341b9af7
Echostrike36 <1reason2live@gmail.com>
2018-08-04 07:37:33 -0500

    Update monitor configuration

--- .config/i3/scripts/desktop16.sh
+++ .config/i3/scripts/desktop16.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x528 --rotate normal --output DP-5 --off --output DP-4 --mode 1920x1080 --pos 4480x0 --rotate left --output DVI-I-1 --off --output DP-2 --mode 2560x1440 --pos 1920x168 --rotate normal --output DP-1 --off --output DP-0 --off
+xrandr --output DP-3 --off --output DP-2.1.8 --mode 2560x1440 --pos 4480x0 --rotate normal --output DP-2 --off --output DP-2.8 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x360 --rotate normal --output DP-5 --off --output DP-4 --off --output DVI-I-1 --off --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
